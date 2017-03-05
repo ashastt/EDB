8ec9179134e97fa633e6cc91b24b128d9812ec82
Asha ST <ashast@Ashas-MacBook-Pro.local>
2017-03-05 17:31:59 -0500

    initial files checkin

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black bean burger', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Glazed Tofu burger', true);
